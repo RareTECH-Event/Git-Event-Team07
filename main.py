b9a28da239139c02eb9da91b695a98a6f7c470f1
masanori <soccer.rai.25@gmail.com>
2025-09-18 21:47:24 +0900

    選択肢2をrenに変更しました

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ def main():
     while True:
         print("選択してください：")
         print("1: きょん")
-        print("2: 選択肢2")
+        print("2: ren")
         print("3: 選択肢3")
         print("q: 終了")
 
@@ -11,7 +11,7 @@ def main():
         if choice == "1":
             print("きょんが選ばれました。")
         elif choice == "2":
-            print("選択肢2が選ばれました。")
+            print("renが選ばれました。")
         elif choice == "3":
             print("選択肢3が選ばれました。")
         elif choice == "q":
